6aa407b9ca4e22fc1e47918c640f2d4faaa3ecab
Shane Emmons <semmons99@gmail.com>
2010-08-09 15:29:15 -0400

    rename #google_rate_for to #get_rate

--- spec/google_currency_spec.rb
+++ spec/google_currency_spec.rb
@@ -8,17 +8,17 @@ describe "GoogleCurrency" do
     @bank = Money::Bank::GoogleCurrency.new
   end
 
-  describe "#google_rate_for" do
+  describe "#get_rate" do
     it "should work for USD->USD" do
-      @bank.google_rate_for('USD', 'USD').should == 1.0
+      @bank.get_rate('USD', 'USD').should == 1.0
     end
 
     it "should work for usd->usd" do
-      @bank.google_rate_for('usd', 'usd').should == 1.0
+      @bank.get_rate('usd', 'usd').should == 1.0
     end
 
     it "should raise an UnknownRate error for an invalid currency" do
-      lambda{@bank.google_rate_for('USD', 'BATMAN')}.should raise_error Money::Bank::UnknownRate
+      lambda{@bank.get_rate('USD', 'BATMAN')}.should raise_error Money::Bank::UnknownRate
     end
   end
 end
