d84cfc75d12c44b46c36399d187b6b10317df18b
Shane Emmons <semmons99@gmail.com>
2010-08-09 14:30:12 -0400

    update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
 require "spec"
-require "google_currency"
 
 Spec::Runner.configure do |config|
 end
