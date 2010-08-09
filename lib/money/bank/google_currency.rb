749a412cc09e4348baeed7910d4a9fd37419b296
Shane Emmons <semmons99@gmail.com>
2010-08-09 15:00:51 -0400

    implement #google_rate_for

--- lib/money/bank/google_currency.rb
+++ lib/money/bank/google_currency.rb
@@ -1,8 +1,14 @@
 require 'money'
+require 'open-uri'
 
 class Money
   module Bank
     class GoogleCurrency < Money::Bank::Base
+      def google_rate_for(from, to)
+        data = eval(URI.parse("http://www.google.com/ig/calculator?hl=en&q=1#{from.upcase}%3D%3F#{to.upcase}").read)
+        raise UnknownRate unless data[:error] == '' or data[:error] == '0'
+        data[:rhs].split(' ')[0].to_f
+      end
     end
   end
 end
