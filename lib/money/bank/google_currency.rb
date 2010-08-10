f8a8f869d05351b27032ed21aef061c2ce95f120
Shane Emmons <semmons99@gmail.com>
2010-08-10 10:32:42 -0400

    compatiability update for Ruby version other than MRI 1.9.1

--- lib/money/bank/google_currency.rb
+++ lib/money/bank/google_currency.rb
@@ -16,7 +16,13 @@ class Money
         from = Currency.wrap(from)
         to   = Currency.wrap(to)
 
-        data = eval(URI.parse("http://www.google.com/ig/calculator?hl=en&q=1#{from.iso_code}%3D%3F#{to.iso_code}").read)
+        data = URI.parse("http://www.google.com/ig/calculator?hl=en&q=1#{from.iso_code}%3D%3F#{to.iso_code}").read
+        data.gsub!(/lhs:/, ':lhs =>')
+        data.gsub!(/rhs:/, ':rhs =>')
+        data.gsub!(/error:/, ':error =>')
+        data.gsub!(/icc:/, ':icc =>')
+        data = eval(data)
+
         raise UnknownRate unless data[:error] == '' or data[:error] == '0'
         data[:rhs].split(' ')[0].to_f
       end
