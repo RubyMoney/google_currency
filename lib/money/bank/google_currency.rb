e5d425f12913da50f86108d8783fba4f11905193
Shane Emmons <semmons99@gmail.com>
2010-08-10 08:59:24 -0400

    implement #get_rate

--- lib/money/bank/google_currency.rb
+++ lib/money/bank/google_currency.rb
@@ -4,6 +4,14 @@ require 'open-uri'
 class Money
   module Bank
     class GoogleCurrency < Money::Bank::VariableExchange
+      attr_reader :rates
+
+      def get_rate(from, to)
+        @mutex.synchronize{
+          @rates[rate_key_for(from, to)] ||= get_google_rate(from, to)
+        }
+      end
+
       def get_google_rate(from, to)
         data = eval(URI.parse("http://www.google.com/ig/calculator?hl=en&q=1#{from.upcase}%3D%3F#{to.upcase}").read)
         raise UnknownRate unless data[:error] == '' or data[:error] == '0'
