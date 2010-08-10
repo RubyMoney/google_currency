f62ca7232b41d27bfac29ac6032e731d9c6c5633
Shane Emmons <semmons99@gmail.com>
2010-08-10 08:59:13 -0400

    spec'd #get_rate

--- spec/google_currency_spec.rb
+++ spec/google_currency_spec.rb
@@ -21,4 +21,21 @@ describe "GoogleCurrency" do
       lambda{@bank.get_google_rate('USD', 'BATMAN')}.should raise_error Money::Bank::UnknownRate
     end
   end
+
+  describe "#get_rate" do
+    it "should use #get_google_rate when rate is unknown" do
+      @bank.should_receive(:get_google_rate).once
+      @bank.get_rate('USD', 'USD')
+    end
+
+    it "should not use #get_google_rate when rate is known" do
+      @bank.get_rate('USD', 'USD')
+      @bank.should_not_receive(:get_google_rate)
+      @bank.get_rate('USD', 'USD')
+    end
+
+    it "should return the correct rate" do
+      @bank.get_rate('USD', 'USD').should == 1.0
+    end
+  end
 end
