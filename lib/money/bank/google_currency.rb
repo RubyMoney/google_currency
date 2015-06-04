d4bf95b48d68d7b056ace46cf0cbb5770c0d2085
Zach Karpinski <zkarpinski@gmail.com>
2015-06-04 07:16:59 -0500

    Using a much simpler fix from @semmons99

--- lib/money/bank/google_currency.rb
+++ lib/money/bank/google_currency.rb
@@ -1,6 +1,11 @@
 require 'money'
 require 'open-uri'
 
+# Fix for https://github.com/RubyMoney/google_currency/issues/38
+# Google considers the Chilean Peso to have 100 Centavos, despite
+# ISO standards indicating otherwise http://en.wikipedia.org/wiki/ISO_4217
+Money::Currency.table[:clp][:subunit_to_unit] = 100
+
 class Money
   module Bank
     # Raised when there is an unexpected error in extracting exchange rates
@@ -170,25 +175,6 @@ class Money
           raise GoogleCurrencyFetchError
         end
       end
-    end
-  end
-end
-
-# Fix for https://github.com/RubyMoney/google_currency/issues/38
-# Google considers the Chilean Peso to have 100 Centavos, despite
-# ISO standards indicating otherwise http://en.wikipedia.org/wiki/ISO_4217
-Money::Currency.class_eval do 
-  class<< self
-    default_table = instance_method(:table)
-
-    define_method(:table) do 
-      default_currencies = default_table.bind(self).call
-
-      unless default_currencies[:clp][:subunit_to_unit] == 100
-        default_currencies[:clp][:subunit_to_unit] = 100
-      end
-
-      return default_currencies
     end
   end
 end
