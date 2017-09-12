2d8fef38c7f37846ef0dbdbf36712b3e86dafd66
Stéphane Mennesson <stephane@buyco.org>
2017-09-12 12:47:03 +0000

    Replaced www.google.com by finance.google.com

--- lib/money/bank/google_currency.rb
+++ lib/money/bank/google_currency.rb
@@ -16,7 +16,7 @@ class Money
     class GoogleCurrency < Money::Bank::VariableExchange
 
 
-      SERVICE_HOST = "www.google.com"
+      SERVICE_HOST = "finance.google.com"
       SERVICE_PATH = "/finance/converter"
 
 
